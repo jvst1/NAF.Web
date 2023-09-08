df52d7f74d1716034e04b86d6b3809607b0d828b
Matheus Tambosi <dev.matheustambosi@gmail.com>
2023-09-08 15:27:12 -0300

    feat: estrutura base pastas e configuracao libs (#1)
    Co-authored-by: matheus.tambosi <matheus.tambosi@fulltechpro.com.br>

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-full flex justify-center items-center">
+    <h1 class="text-red-800">Sem autenticação</h1>
+    <button @click="login">Logar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'default',
+  methods: {
+    login() {
+      this.$store.dispatch('setUser', { name: 'Matheus', role: 1 })
+
+      this.$router.push({ name: 'dashboard' })
+    }
+  }
+}
+</script>
